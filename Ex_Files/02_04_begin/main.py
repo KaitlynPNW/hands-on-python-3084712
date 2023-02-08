f12f89c78ceb4b1ac4ffaa445c2d1fedd1d42680
KaitlynPNW <121985000+KaitlynPNW@users.noreply.github.com>
2023-02-08 17:30:19 +0000

    online learning through lesson 2-4

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -1,21 +1,30 @@
 NAMES = ["John", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
-i = 0
+print("traditional while loop")
+
+i = 0 #traditional while loop
 while i < len(NAMES):
     print(NAMES[i], AGES[i])
     i += 1
 
-for name in NAMES:
+print("\n for in loop")
+
+# more popular loop
+for name in NAMES: #for each name in names
     print(name)
 
-for name, age in zip(NAMES, AGES):
+for name, age in zip(NAMES, AGES): #iterate through two collections at once, this does not create a new list in memory
     print(f"{name} {age}")
 
-for name in reversed(NAMES):
+for name in reversed(NAMES): #won't take up space in memory
     print(name)
 
 for i in range(5):
     print(i)
 
-# enumerate
+# enumerate -- so you know what index you are on in the for in loops
+print("\n enumerate")
+for i, name in enumerate(NAMES):
+    print(f"{i} {name}")
+
