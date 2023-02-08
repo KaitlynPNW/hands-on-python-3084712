100379f0db954888901e6bca321b8c15f9076754
KaitlynPNW <121985000+KaitlynPNW@users.noreply.github.com>
2023-02-08 18:23:27 +0000

    chapter 2 complete

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -4,8 +4,8 @@ AGES = [20, 21, 22, 23]
 JOHN = NAMES[0] #can access like an array in other languages
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2] #names to the left of 2
-GEORGE_RINGO = NAMES[2:] #get 2 names to the right of :
+JOHN_PAUL = NAMES[:2] #names to the left of 2 (last two items of the list)
+GEORGE_RINGO = NAMES[2:] #get 2 names to the right of : (first two items of the list)
 REVERSE = NAMES[::-1] #start:stop:step
 EVERY_OTHER = NAMES[::2]
 
--- Ex_Files/02_05_begin/main.py
+++ Ex_Files/02_05_begin/main.py
@@ -7,15 +7,15 @@ STAGING = "staging"
 CODE_SPACE = "code_space"
 LOCAL = "local"
 
-current_env = os.environ.get("ENV_NAME", DEVELOPMENT)
+current_env = os.environ.get("ENV_NAME", DEVELOPMENT) #grab things from the enviroment; will default to development if no current enviroment
 
-if current_env == DEVELOPMENT:
+if current_env == DEVELOPMENT: #checking
     print("Development environment")
 elif current_env == PRODUCTION:
     print("Production environment")
 elif current_env == STAGING:
     print("Staging environment")
-elif current_env in [CODE_SPACE, LOCAL]:
+elif current_env in [CODE_SPACE, LOCAL]: #checking against multiple things
     print("Codespace or local environment")
 else:
     print("Unknown environment")
